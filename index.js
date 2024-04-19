3a3d94dcc2714456a35c18efe7326bc3e2bc1baf
Keltrim <maltrum1991@hotmail.com>
2024-04-19 23:44:33 +0300

    pr-task3

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { router as bookRouter } from './routes/book-router.js';
 import { router as userRouter } from './routes/user-router.js';
+import { router as indexRouter } from './routes/index.js';
+import errorMiddleware from './middleware/error.js';
 
 const app = express();
-app.use(express.json());
-app.use(bookRouter);
+app.set('view engine', 'ejs');
+app.use('/', indexRouter);
+app.use('/books', bookRouter);
 app.use(userRouter);
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT)
+app.listen(PORT) 
